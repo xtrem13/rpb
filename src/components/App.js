8b674665c73d59506bcc9cdd2b857eb831d2754d
Abdumalik <axtrem13@gmail.com>
2019-08-05 13:12:00 +0500

    file structure optimized

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from '../assets/images/logo.svg';
+import './App.css';
+
+export default function App() {
+  return (
+    <div className="App">
+     
+    </div>
+  );
+}
+
+
