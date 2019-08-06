730ee430ab19c8950951bfd8eb29b81f4cb644d5
Shokhrukh2606 <shakhrukh7ielts@mail.ru>
2019-08-07 01:32:43 +0500

    some changes

--- src/components/Presenter/presenter.js
+++ src/components/Presenter/presenter.js
@@ -1,9 +1,36 @@
-import React from 'react';
-
+import React, { useState } from 'react';
 
 const Presenter=()=> {
+  const [uniT, setUnit]=useState('px');
+  const [height, setHeight]=useState(25+uniT);
+  const [width, setWidth]=useState(25+uniT);
+  const [border, setborder]=useState(1+uniT+' dotted black');
+  /* 
+  function PageComponent() {
+  const [count, setCount] = useState(0);
+  const increment = () => {
+    setCount(count + 1)
+  }
+
   return (
-    <section>
+    <div className="App">
+      <ChildComponent onClick={increment} count={count} />         
+      <h2>count {count}</h2>
+      (count should be updated from child)
+    </div>
+  );
+}
+
+const ChildComponent = ({ onClick, count }) => {
+  return (
+    <button onClick={onClick}>
+       Click me {count}
+    </button>
+  )
+};
+  */
+  return (
+    <section >
       <div className="container">
         <div className="row">
           <div className="col-md-12">
@@ -14,4 +41,5 @@ const Presenter=()=> {
     </section>
   );
 }
+
 export default Presenter
--- src/components/Wrapper/wrapper.js
+++ src/components/Wrapper/wrapper.js
@@ -1,43 +1,48 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import Element from '../Element'
+import './wrapper.css'
 
-const Wrapper=()=> {
-  const [elements, addElement]=useState([0]);
+const Wrapper = () => {
+  const [elements, addElement] = useState([0]);
 
-  var body=elements.map(i=>{
-    return <Element key={i}/>
+  var body = elements.map(i => {
+    return <Element key={i} />
   })
-  const addSection=(e)=>{
-    let newElement=elements[elements.length-1]+1
-    addElement([... elements, newElement])
+  const addSection = (e) => {
+    let newElement = elements[elements.length - 1] + 1
+    addElement([...elements, newElement])
   }
-  const exportIt=(e)=>{
-    let title="index.html"
-    let text=document.getElementsByClassName("forExport")[0].innerHTML;
-    download(title, text)   
+  const exportIt = (e) => {
+    let title = "index.html"
+    let text = document.getElementsByClassName("forExport")[0].innerHTML;
+    download(title, text)
   }
   function download(filename, text) {
     var pom = document.createElement('a');
     pom.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
     pom.setAttribute('download', filename);
-    
-   if (document.createEvent) {
-        var event = document.createEvent('MouseEvents');
 
-        event.initEvent('click', true, true);
-        pom.dispatchEvent(event);
+    if (document.createEvent) {
+      var event = document.createEvent('MouseEvents');
+
+      event.initEvent('click', true, true);
+      pom.dispatchEvent(event);
     }
     else {
-        pom.click();
+      pom.click();
     }
-  }	
+  }
   return (
     <div>
       <div className="forExport">
         {body}
-      </div> 
-      <button onClick={addSection}>Add Section</button>
-      <button onClick={exportIt}>Export to HTML</button>
+      </div>
+      <div className="builderController">
+        <div className="builderMain">
+          <button onClick={addSection}>Add Section</button>
+          <button onClick={exportIt}>Export to HTML</button>
+        </div>
+      </div>
     </div>
   );
 }
